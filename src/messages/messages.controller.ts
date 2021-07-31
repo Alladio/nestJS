a8c6575c4a50e87833884933c725a7fed2540119
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-07-31 15:51:58 -0300

    CREATE: get, put, post

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Message } from './message';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
     constructor(private messagesService: MessagesService){}
-
+    //Buscar todos
     @Get()
     findAll() {
         return this.messagesService.messages;
+    }
+
+    //Buscar por ID
+    @Get(':id')
+    findById(@Param() params){
+        return this.messagesService.findById(+params.id);
+    }
+
+    //Criar
+    @Post()
+    create(@Body() message:Message){
+        console.log({message});
+        return this.messagesService.create(message);
+    }
+
+    //Editar
+    @Put(':id')
+    update (@Param() params, @Body() message:Message){
+        console.log({message});
+        return this.messagesService.update(+params.id, message);
     }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Message } from './message';
 
 @Injectable()
 export class MessagesService {
     //Simulando uma tabela
-    public messages =[
+    public messages: Message[] = [
         {
             id: 1,
-            text: 'olá'
+            text: 'Mensagem 1'
         },
         {
             id: 2,
-            text: 'tchau'
+            text: 'Mensagem 2'
         }
     ];
     //Simuando um consulta no DB
     findAll(){
         return this.messages;
+    }
+
+    findById(id:number){
+        return this.messages.find( (message) => message.id ===id);
+
+    }
+
+    create(message: Message ){
+        return this.messages.push(message);
+    }
+
+    update(id:number, message: Message){
+        const index = this.messages.findIndex((message) => message.id ===id);
+        this.messages[index]=message;
+        return message;
     }
 }
